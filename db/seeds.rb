fed8f5f28de00eac1f0df00dfa49c0f705aef4cd
84tghuynh <giangbrvt@gmail.com>
2020-11-07 21:10:53 -0600

    Changes customer.rb of active_admin and seed.rb

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,4 +5,27 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
-AdminUser.create!(email: 'admin@example.com', password: 'password', password_confirmation: 'password') if Rails.env.development?
+Customer.delete_all
+AdminUser.delete_all
+
+fullname:string phonenumber:string email:string notes:text
+
+10.times do
+  Customer.create(
+    fullname: Faker::Name.unique.name,
+    phonenumber:Faker::PhoneNumber.unique.cell_phone,
+    email: Faker::Internet.unique.email,
+    notes: Faker::Coffee.unique.notes
+
+  )
+
+  #query =URI.encode_www_form_component([customer.fullname])
+  #downloaded_image = URI.open("https://source.unsplash.com/600x600/?#{query}")
+  #customer.image.attach(io:downloaded_image, filename: "m-#{[customer.fullname]}.jpg")
+end
+
+puts "Created #{Customer.count} customers."
+
+if Rails.env.development?
+  AdminUser.create!(email: "admin@example.com", password: "password", password_confirmation: "password")
+end
