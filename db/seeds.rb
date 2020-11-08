09e0419b002ad17b661beb3adb3eb46033488854
84tghuynh <giangbrvt@gmail.com>
2020-11-07 21:23:08 -0600

    changed seed.db

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,23 +5,22 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+require "faker"
+
 Customer.delete_all
 AdminUser.delete_all
 
-fullname:string phonenumber:string email:string notes:text
-
 10.times do
   Customer.create(
-    fullname: Faker::Name.unique.name,
-    phonenumber:Faker::PhoneNumber.unique.cell_phone,
-    email: Faker::Internet.unique.email,
-    notes: Faker::Coffee.unique.notes
-
+    fullname:    Faker::Name.unique.name,
+    phonenumber: Faker::PhoneNumber.unique.cell_phone.to_s,
+    email:       Faker::Internet.unique.email,
+    notes:       Faker::Coffee.unique.notes
   )
 
-  #query =URI.encode_www_form_component([customer.fullname])
-  #downloaded_image = URI.open("https://source.unsplash.com/600x600/?#{query}")
-  #customer.image.attach(io:downloaded_image, filename: "m-#{[customer.fullname]}.jpg")
+  # query =URI.encode_www_form_component([customer.fullname])
+  # downloaded_image = URI.open("https://source.unsplash.com/600x600/?#{query}")
+  # customer.image.attach(io:downloaded_image, filename: "m-#{[customer.fullname]}.jpg")
 end
 
 puts "Created #{Customer.count} customers."
