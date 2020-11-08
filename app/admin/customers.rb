6462128531b4c2886cb75fcde682d7ddb0df97be
84tghuynh <giangbrvt@gmail.com>
2020-11-07 20:49:23 -0600

    Register Customer with Active_admin

---
+++ app/admin/customers.rb
@@ -0,0 +1,18 @@
+ActiveAdmin.register Customer do
+
+  # See permitted parameters documentation:
+  # https://github.com/activeadmin/activeadmin/blob/master/docs/2-resource-customization.md#setting-up-strong-parameters
+  #
+  # Uncomment all parameters which should be permitted for assignment
+  #
+  # permit_params :fullname, :phonenumber, :email, :notes
+  #
+  # or
+  #
+  # permit_params do
+  #   permitted = [:fullname, :phonenumber, :email, :notes]
+  #   permitted << :other if params[:action] == 'create' && current_user.admin?
+  #   permitted
+  # end
+  
+end
