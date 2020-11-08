48a4188d4368aa47ec12bcaf24d8f1de5720be29
84tghuynh <giangbrvt@gmail.com>
2020-11-08 10:01:02 -0600

    Changing route to show/id, and seeds.rb to get photos

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,8 @@
 Rails.application.routes.draw do
-  get 'customers/index'
-  get 'customers/show'
+  # get 'customers/index'
+  # get 'customers/show'
+  get "customers",     to: "customers#index", as: "customers" # customers_path
+  get "customers/:id", to: "customers#show",  as: "customer"
   devise_for :admin_users, ActiveAdmin::Devise.config
   ActiveAdmin.routes(self)
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
--- db/seeds.rb
+++ db/seeds.rb
@@ -11,16 +11,16 @@ Customer.delete_all
 AdminUser.delete_all
 
 10.times do
-  Customer.create(
+  customer = Customer.create(
     fullname:    Faker::Name.unique.name,
     phonenumber: Faker::PhoneNumber.unique.cell_phone.to_s,
     email:       Faker::Internet.unique.email,
     notes:       Faker::Coffee.unique.notes
   )
 
-  # query =URI.encode_www_form_component([customer.fullname])
-  # downloaded_image = URI.open("https://source.unsplash.com/600x600/?#{query}")
-  # customer.image.attach(io:downloaded_image, filename: "m-#{[customer.fullname]}.jpg")
+  query = URI.encode_www_form_component([customer.fullname])
+  downloaded_image = URI.open("https://source.unsplash.com/600x600/?#{query}")
+  customer.image.attach(io: downloaded_image, filename: "m-#{[customer.fullname]}.jpg")
 end
 
 puts "Created #{Customer.count} customers."
